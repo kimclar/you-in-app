63820687fe1b3effdf3e847c994716015c44123a
clara-kim <kim.clar@husky.neu.edu>
2019-11-26 11:50:50 -0500

    added mock data (15 friends) and implemented friends list

--- app/components/MyFriends.vue
+++ app/components/MyFriends.vue
@@ -4,20 +4,42 @@
             <Label text="My Friends"></Label>
         </ActionBar>
 
-        <GridLayout class="page__content">
-            <Label class="page__content-icon fas" text.decode="&#xf500;"></Label>
-            <Label class="page__content-placeholder" :text="message"></Label>
-        </GridLayout>
+        <ListView for="friend in friends">
+            <v-template>
+                <StackLayout orientation="horizontal">
+                    <Label :text="friend.nickname" textWrap="true"></Label>
+                </StackLayout>
+            </v-template>
+        </ListView>
     </Page>
 </template>
 
 <script>
+import friendsData from './mockData/friendsData.json';
+
 export default {
     data: () => {
         return {
-            message: "All your Friends!"
+            friends: friendsData
         };
-    }
+    },
+/*    methods: {
+        onItemTap (args) {
+            const view = args.view;
+            const page = view.page;
+            const tappedItem = view.bindingContext;
+
+            this.$navigateTo(ItemDetails, {
+                props: {
+                    context: tappedItem,
+                    animated: true,
+                    transition: {
+                        name: "slide",
+                        duration: 200,
+                        curve: "ease"
+                    }}});
+        }
+    }*/
 }
 </script>
 
