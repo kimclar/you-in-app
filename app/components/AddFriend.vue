85190a4c94733964c945095d9145ae3076b71f01
shravyaraj <raj.s@husky.neu.edu>
2019-11-30 21:29:28 -0500

    added send friend request

--- app/components/AddFriend.vue
+++ app/components/AddFriend.vue
@@ -1,7 +1,7 @@
 <template>
     <ScrollView>
 
-        <StackLayout width="600" height="600" class="p-10">
+        <StackLayout width="700" height="600" class="p-10">
 
             <Label class="pull-right h2 action label icon fas" text.decode="&#xf057;" @tap="$modal.close()" />
 
@@ -11,8 +11,10 @@
                 <SearchBar hint="Enter username" @textChange="onTextChanged"/>
                 <ListView for="friend in friends" height="900">
                 <v-template>
-                    <Label :text="friend.username" textWrap="true"></Label>
-                    <Button class="pull-right" width="80" text="Send"></Button>
+                    <StackLayout>
+                    <Label :text="friend.username" class="h3 font-weight-bold"></Label>
+                    <Button class="h5 bg-primary pull-right" text="Send Request" @tap="sendRequest"></Button>
+                    </StackLayout>
                 </v-template>
             </ListView>
 
@@ -37,7 +39,15 @@
         },
         methods:{
             onTextChanged(){
-            }
+            },
+            sendRequest () {
+                var dialogs = require("tns-core-modules/ui/dialogs");
+                dialogs.alert({
+                                  title: "",
+                                  message: "Request Sent!",
+                                  okButtonText: "OK"
+                              })
+            },
         }
     }
 </script>
