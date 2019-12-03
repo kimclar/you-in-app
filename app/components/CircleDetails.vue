d3b4511ef57b25620a259a3f31cffef3fb712ab5
clara-kim <kim.clar@husky.neu.edu>
2019-12-02 21:31:12 -0500

    delete circle implemented

--- app/components/CircleDetails.vue
+++ app/components/CircleDetails.vue
@@ -23,7 +23,7 @@
             <Button width="200" class="h3" color="white"
                     backgroundColor="blue" text="Edit" @tap="sendRequest"></Button>
             <Button width="200" class="h3" color="white"
-                    backgroundColor="red" text="Delete" @tap="deleteCircle"></Button>
+                    backgroundColor="red" text="Delete" @tap="deleteCircle(circle)"></Button>
 
 
     </FlexboxLayout>
@@ -45,8 +45,19 @@
             }
         },
         methods: {
-            deleteCircle(){
-                this.$showModal(ConfirmDeleteCircle)
+            deleteCircle(passCircle){
+                this.$showModal(ConfirmDeleteCircle, {
+                    props: {
+                        circle: passCircle,
+                        animated: true,
+                        transition: {
+                            name: "slide",
+                            duration: 200,
+                            curve: "ease"
+                        }
+                    }
+                })
+                this.$modal.close();
             }
         }
     };
